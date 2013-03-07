920b1e83baf181cacb4c5fcbb419b9e88ef34e63
Scott Davis <stdavis@utah.gov>
2013-03-07 08:29:46 -0700

    More build stuff.

--- src/app/run.js
+++ src/app/run.js
@@ -19,11 +19,6 @@
                 name: 'ijit',
                 location: projectUrl + 'ijit'
             }
-        ],
-        // This is a hack. In order to allow app/main and app/run to be built together into a single file, a cache key needs
-        // to exist here in order to force the loader to actually process the other modules in the file. Without this hack,
-        // the loader will think that code for app/main has not been loaded yet and will try to fetch it again, resulting in
-        // a needless extra HTTP request.
-        cache: {}
+        ]
     }, ['app']);
 })();
