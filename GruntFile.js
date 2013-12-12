0e0f1966327106627c5fc4d8cd444f3dbe386bb3
Scott Davis <scott.sheri@gmail.com>
2013-12-12 12:52:27 -0700

    Added live reload and reorganized grunt file.

--- GruntFile.js
+++ GruntFile.js
@@ -1,47 +1,68 @@
 module.exports = function(grunt) {
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jasmine: {
-      // for embedded map projects...
-      // app: {
-      //   src: ['src/EmbeddedMapLoader.js'],
-      //   options: {
-      //     specs: ['src/app/tests/spec/*.js']
-      //   }
-      // }
-      
-      // for regular apps...
-      app: {
-        src: ['src/app/run.js'],
-        options: {
-        specs: ['src/app/tests/spec/*.js'],
-        vendor: [
-          'src/app/tests/jasmineTestBootstrap.js',
-          'http://js.arcgis.com/3.6/'
-          ]
+    var jsFiles = 'src/app/**/*.js';
+    var otherFiles = [
+        'src/app/**/*.html',
+        'src/app/**/*.css',
+        'src/index.html'
+    ];
+    var gruntFile = 'GruntFile.js';
+    var internFile = 'tests/intern.js';
+    var packageFile = 'package.json';
+    var jshintFiles = [jsFiles, gruntFile, internFile, packageFile];
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jasmine: {
+            // for embedded map projects...
+            // app: {
+            //   src: ['src/EmbeddedMapLoader.js'],
+            //   options: {
+            //     specs: ['src/app/tests/spec/*.js']
+            //   }
+            // }
+
+            // for regular apps...
+            app: {
+                src: ['src/app/run.js'],
+                options: {
+                    specs: ['src/app/tests/spec/*.js'],
+                    vendor: [
+                        'src/app/tests/jasmineTestBootstrap.js',
+                        'http://js.arcgis.com/3.6/'
+                    ]
+                }
+            }
+        },
+        jshint: {
+            files: jshintFiles,
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        watch: {
+            jshint: {
+                files: jshintFiles,
+                tasks: ['jshint']
+            },
+            src: {
+                files: jshintFiles.concat(otherFiles),
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        connect: {
+            uses_defaults: {}
         }
-      }
-    },
-    jshint: {
-      files: ['src/app/**/*.js'],
-      options: {jshintrc: '.jshintrc'}
-    },
-    watch: {
-      files: ['src/app/**/*.js'],
-      tasks: ['jasmine:app:build', 'jshint']
-    },
-    connect: {
-      uses_defaults: {}
-    }
-  });
+    });
 
-  // Register tasks.
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+    // Register tasks.
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-  // Default task.
-  grunt.registerTask('default', ['jasmine:app:build', 'jshint', 'connect', 'watch']);
+    // Default task.
+    grunt.registerTask('default', ['jasmine:app:build', 'jshint', 'connect', 'watch']);
 };
