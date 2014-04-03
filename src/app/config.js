4d51ef6bacf162dde809e67ee7e2b8a9bc6c4840
Scott Davis <stdavis@utah.gov>
2014-04-03 13:56:53 -0600

    Replaced vector url that was mistakenly removed. Closes #37

--- src/app/config.js
+++ src/app/config.js
@@ -25,7 +25,8 @@ define(['dojo/has'], function (has) {
         utm12wkt: 'PROJCS["NAD83 / UTM zone 12N",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",-111],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",0],AUTHORITY["EPSG","26912"],AXIS["Easting",EAST],AXIS["Northing",NORTH]]',
 
         urls: {
-            search: 'http://api.mapserv.utah.gov/api/v1/search/{0}/{1}'
+            search: 'http://api.mapserv.utah.gov/api/v1/search/{0}/{1}',
+            vector: 'http://mapserv.utah.gov/arcgis/rest/services/BaseMaps/Vector/MapServer'
         },
 
         fieldNames: {
