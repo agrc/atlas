8f8857f48d46ff145595f8d4122c5c4b6f161e3c
Scott Davis <stdavis@utah.gov>
2014-04-08 15:40:28 -0600

    Basic layout for Search widget.
    Added child widgets for all searches except site name and program id. Need to talk to Harold about those.
    
    Got stack container wired to drop down.
    
    Closes #19.

---
+++ src/app/search/tests/spec/SpecCity.js
@@ -0,0 +1,37 @@
+require([
+    'app/search/City',
+
+    'dojo/_base/window',
+
+    'dojo/dom-construct'
+], function(
+    WidgetUnderTest,
+
+    win,
+
+    domConstruct
+) {
+    describe('app/search/City', function() {
+        var widget;
+        var destroy = function (widget) {
+            widget.destroyRecursive();
+            widget = null;
+        };
+
+        beforeEach(function() {
+            widget = new WidgetUnderTest(null, domConstruct.create('div', null, win.body()));
+        });
+
+        afterEach(function() {
+            if (widget) {
+                destroy(widget);
+            }
+        });
+
+        describe('Sanity', function() {
+            it('should create a City', function() {
+                expect(widget).toEqual(jasmine.any(WidgetUnderTest));
+            });
+        });
+    });
+});
