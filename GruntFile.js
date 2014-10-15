2f41edd95a98ba41b30d98c8cdbea813d9f7eb33
Scott Davis <stdavis@utah.gov>
2014-10-15 13:00:02 -0600

    Moved compress to deploy

--- GruntFile.js
+++ GruntFile.js
@@ -241,8 +241,7 @@ module.exports = function(grunt) {
         'newer:imagemin:main',
         'dojo:prod',
         'copy:main',
-        'processhtml:main',
-        'compress:main'
+        'processhtml:main'
     ]);
     grunt.registerTask('build-stage', [
         'clean:build',
@@ -250,16 +249,17 @@ module.exports = function(grunt) {
         'newer:imagemin:main',
         'dojo:stage',
         'copy:main',
-        'processhtml:main',
-        'compress:main'
+        'processhtml:main'
     ]);
     grunt.registerTask('deploy-prod', [
         'clean:deploy',
+        'compress:main',
         'sftp:prod',
         'sshexec:prod'
     ]);
     grunt.registerTask('deploy-stage', [
         'clean:deploy',
+        'compress:main',
         'sftp:stage',
         'sshexec:stage'
     ]);
