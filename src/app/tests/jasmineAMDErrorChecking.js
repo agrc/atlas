711209a5bc88674954ee688cebb1a666f2be038c
Scott Davis <stdavis@utah.gov>
2014-11-28 13:56:48 -0700

    amd load test should wait for xstyle to finish

--- src/app/tests/jasmineAMDErrorChecking.js
+++ src/app/tests/jasmineAMDErrorChecking.js
@@ -1,7 +1,30 @@
 require(['dojo/domReady!'], function () {
     describe('AMD Loader', function () {
-        it('dependencies loaded successfully', function () {
-            expect(require.waiting).toEqual({});
+        it('dependencies loaded successfully', function (done) {
+            var pause = 100;
+
+            var hasProps = function (obj) {
+                for (var prop in obj) {
+                    if (obj.hasOwnProperty(prop)) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+
+            var check = function () {
+                console.log('require.waiting', require.waiting);
+                if (hasProps(require.waiting)) {
+                    // Keep checking until jasmine default timeout. 
+                    // xstyle/css seems to make this take an extra few milliseconds.
+                    window.setTimeout(check, pause);
+                } else {
+                    expect(require.waiting).toEqual({});
+                    done();
+                }
+            };
+
+            check();
         });
     });
 });
