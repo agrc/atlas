33a1cb327c97c77dd1a0064b946a23d7b4743cb6
Scott Davis <stdavis@utah.gov>
2015-03-02 08:46:15 -0700

    increase sauce tunnel timeout

--- GruntFile.js
+++ GruntFile.js
@@ -115,7 +115,7 @@ module.exports = function(grunt) {
         secrets,
         sauceConfig = {
             urls: ['http://127.0.0.1:8000/_SpecRunner.html'],
-            tunnelTimeout: 20,
+            tunnelTimeout: 120,
             build: process.env.TRAVIS_JOB_ID,
             browsers: browsers,
             testname: 'atlas',
