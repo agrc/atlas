86605c1181cc9dd1fe1ec25af6a3e0fc63646d11
steveoh <sgourley@utah.gov>
2018-01-09 11:46:59 -0700

    switch to new dem
    the fields might need to be updated also. I was not able to use the api to query currently so this is not ready for a merge.

--- src/app/config.js
+++ src/app/config.js
@@ -74,7 +74,7 @@ define([
             municipalities: 'SGID10.Boundaries.Municipalities',
             landOwnership: 'SGID10.Cadastre.LandOwnership',
             nationalGrid: 'SGID10.Indices.NationalGrid',
-            dem: 'SGID10.RASTER.DEM_10METER'
+            dem: 'SGID10.RASTER.USGS_DEM_10METER'
         }
     };
 
