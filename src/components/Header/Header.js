0faa43b95902a78f0c11b25ee5bf9362df4d7ec1
Scott Davis <stdavis@utah.gov>
2018-12-07 13:03:32 -0700

    feat: hide sidebar and AGRC logo by default on smaller screens Ref: #212

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import './Header.css';
 import logo from './agrc_logo.jpg';
+import config from '../../config';
 
 export default class Header extends PureComponent {
   render() {
@@ -10,7 +11,7 @@ export default class Header extends PureComponent {
           <span>{this.props.title}</span>
           <a className="heading__version" href="/changelog.html" target="_blank" rel="noopener">{this.props.version}</a>
         </h1>
-        <img src={logo} className="heading__img" alt="agrc logo" />
+        {window.innerWidth >= config.MIN_DESKTOP_WIDTH && <img src={logo} className="heading__img" alt="agrc logo" />}
       </div>
     )
   }
