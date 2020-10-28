989bc12ae8e2b8355519d9813e374f3bb10f476e
steveoh <sgourley@utah.gov>
2020-10-28 12:11:23 -0600

    chore: add esri css

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '@arcgis/core/assets/esri/themes/light/main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
