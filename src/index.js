b35983d8db5f44d916edb1f62bb27588922db2bd
steveoh <sgourley@utah.gov>
2020-12-01 14:34:31 -0700

    chore: add new CRA defaults

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import '@arcgis/core/assets/esri/themes/light/main.css';
 
 ReactDOM.render(
@@ -13,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals();
