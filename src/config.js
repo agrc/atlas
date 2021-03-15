497f7f887f2c74416cf89f6ac431eeacc24a8179
steveoh <sgourley@utah.gov>
2021-03-15 12:35:12 -0400

    style: eslint fixes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
-export default {
+const config = {
   MIN_DESKTOP_WIDTH: 768,
   WEB_MERCATOR_WKID: 3857,
   MARKER_FILL_COLOR: [130, 65, 47, 0.5],
-  MARKER_OUTLINE_COLOR: [230, 126, 21, 0.7]
+  MARKER_OUTLINE_COLOR: [230, 126, 21, 0.7],
 };
+
+export default config;
