f05ebe2b340ef0f83534988850860af0100d4b08
steveoh <sgourley@utah.gov>
2022-07-21 17:44:58 -0600

    fix: update components

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-is', 'prop-types', 'react-fast-compare', 'warning', 'react-dom', 'classnames'],
-  },
   base: './',
   test: {
     globals: true,
