2824a161d01895c51307a644861e58e0c4927207
steveoh <sgourley@utah.gov>
2022-07-21 20:23:44 -0600

    feat: add source maps for dev builds

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    base: './',
+    build: {
+      sourcemap: env.VITE_SOURCE_MAPS,
+    },
+  };
 });
