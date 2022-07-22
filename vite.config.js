65024bf97eb115f59c31d7e93281582b26080f58
steveoh <sgourley@utah.gov>
2022-07-21 20:28:52 -0600

    Revert "feat: add source maps for dev builds"
    This reverts commit 2824a161d01895c51307a644861e58e0c4927207.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    base: './',
-    build: {
-      sourcemap: env.VITE_SOURCE_MAPS,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: './',
 });
