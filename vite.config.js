fd86f7043af08ec26bf6e48b0dd2a5c5fa5b7c17
stdavis <stdavis@utah.gov>
2022-09-06 16:34:29 -0600

    fix: display version number from package json
    Co-authored-by: steveoh <sgourley@utah.gov>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import loadVersion from 'vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',
+  plugins: [react(), loadVersion()],
 });
