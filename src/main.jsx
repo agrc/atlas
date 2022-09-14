df869d8eeaee177604ea83ca2fcc3ed5e07ba707
steveoh <sgourley@utah.gov>
2022-09-13 18:47:06 -0600

    feat: use Google Analytics 4 with env variable

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,28 @@ import '@arcgis/core/assets/esri/themes/light/main.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { FirebaseAppProvider } from 'reactfire';
+import App from './App.jsx';
 import './index.css';
+
+let firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
+
+if (import.meta.env.VITE_FIREBASE_CONFIG) {
+  firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>
 );
