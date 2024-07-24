350717affcaf8cc0541093627a75ae8be9c91223
steveoh <sgourley@utah.gov>
2024-07-23 20:08:26 -0600

    chore: correct content to include typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./node_modules/@ugrc/**/*.{jsx,js}', './index.html', './src/**/*.{jsx,js}'],
+  content: ['./node_modules/@ugrc/**/*.{tsx,jsx,js}', './index.html', './src/**/*.{tsx,jsx,js}'],
   presets: [require('@ugrc/tailwind-colors')],
   theme: {
     extend: {
