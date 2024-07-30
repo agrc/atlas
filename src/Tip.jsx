7f890e88f4d90c663217c3447ecad8cbff8c880e
steveoh <sgourley@utah.gov>
2024-07-30 16:38:33 -0600

    refactor: create tip component

---
+++ src/Tip.jsx
@@ -0,0 +1,22 @@
+export const Tip = ({ title = 'Tip', children }) => (
+  <div className="border border-zinc-200 dark:border-zinc-700 rounded">
+    <p className="flex items-center gap-3 pb-2 pl-4 pt-4 text-3xl font-semibold text-primary-900 dark:text-accent-600">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+        ></path>
+      </svg>
+      <span>{title}</span>
+    </p>
+    <div className="m-0 px-4 pb-4">{children}</div>
+  </div>
+);
