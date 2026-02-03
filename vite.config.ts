fa2c9f9484e3f9a358b161efc120006d35f283aa
stdavis <stdavis@utah.gov>
2026-02-03 06:51:47 -0700

    fix: convert to arcgis components

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import loadVersion from 'vite-plugin-package-version';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
   resolve: {
     // this is only applicable when pnpm-linking the utah-design-package
-    dedupe: ['firebase', '@arcgis/core'],
+    // dedupe: ['firebase', '@arcgis/core', '@arcgis/lumina', '@arcgis/map-components', 'react', 'react-dom'],
   },
   test: {
     environment: 'happy-dom',
